60ef09f844a7dc1873d14210ee05693502725952
Przemyslaw Delimata <delimata.ps@gmail.com>
2022-06-09 23:28:18 +0200

    JSON recipe added

--- src/main/java/com/infoshareacademy/recipe/Recipe.java
+++ src/main/java/com/infoshareacademy/recipe/Recipe.java
@@ -11,6 +11,47 @@ public class Recipe {
     private String name;
     private String description;
     private Map<String, Double> neccesaryProducts;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Map<String, Double> getNeccesaryProducts() {
+        return neccesaryProducts;
+    }
+
+    public void setNeccesaryProducts(Map<String, Double> neccesaryProducts) {
+        this.neccesaryProducts = neccesaryProducts;
+    }
+
+    public int getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(int preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+
+    public List<FoodPreferences> getMealPreferences() {
+        return mealPreferences;
+    }
+
+    public void setMealPreferences(List<FoodPreferences> mealPreferences) {
+        this.mealPreferences = mealPreferences;
+    }
+
     private int preparationTime;
     private List<FoodPreferences> mealPreferences;
 
