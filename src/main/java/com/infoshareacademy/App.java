10c0f1a9792cf996e8d934a4b9fe509e8ed04fe7
Przemyslaw Delimata <delimata.ps@gmail.com>
2022-07-03 20:17:07 +0200

    shoppingListCreate method  created -> all Recipe vs Fridge

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,12 +1,18 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.service.RecipeService;
+import com.infoshareacademy.service.ShoppingListService;
+
 import java.io.IOException;
 import java.util.*;
 
 public class App {
     public static void main(String[] args) throws IOException {
         System.out.println("Team name: Java The Hutt");
-        Menu menu = new Menu();
-        menu.mainMenu();
+
+
+        ShoppingListService shoppingListService = new ShoppingListService();
+        shoppingListService.createShoppingList();
+
     }
 }
