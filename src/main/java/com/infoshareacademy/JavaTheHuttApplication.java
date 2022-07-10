74469f43e9d86e47c38ee379bf13d6035da82d48
Mikołaj Klimek <mikolaj.klimek.25@gmail.com>
2022-07-10 13:12:24 +0200

    Spring boot app refactored

--- src/main/java/com/infoshareacademy/JavaTheHuttApplication.java
+++ src/main/java/com/infoshareacademy/JavaTheHuttApplication.java
@@ -1,4 +1,4 @@
-package com.infoshareacademy.javathehutt;
+package com.infoshareacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
