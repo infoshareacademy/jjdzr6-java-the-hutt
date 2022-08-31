d978094803a89b7bdea28abf03638588ddedf2ab
Mikołaj Klimek <mikolaj.klimek.25@gmail.com>
2022-08-31 21:01:40 +0200

    Fridge Entity added

--- src/main/java/com/infoshareacademy/entity/fridge/Fridge.java
+++ src/main/java/com/infoshareacademy/entity/fridge/Fridge.java
@@ -1,9 +1,41 @@
 package com.infoshareacademy.entity.fridge;
 
 
+import com.infoshareacademy.entity.product.Product;
+
 import javax.persistence.*;
+import java.util.List;
 
-
+@Entity
 public class Fridge {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long fridgeId;
+
+    private List<Product> productList;
+
+
+    public  Fridge(){
+
+    }
+    public Fridge(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public Long getFridgeId() {
+        return fridgeId;
+    }
+
+    public void setFridgeId(Long fridgeId) {
+        this.fridgeId = fridgeId;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
 }
