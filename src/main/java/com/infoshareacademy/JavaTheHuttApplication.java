5ea0d7d9dc1c2543aa2f53288c12e1f0e95b7eb2
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-09-22 19:12:38 +0200

    Delete implementation of CommandLineRunner class, exception and run(); method.

--- src/main/java/com/infoshareacademy/JavaTheHuttApplication.java
+++ src/main/java/com/infoshareacademy/JavaTheHuttApplication.java
@@ -1,23 +1,15 @@
 package com.infoshareacademy;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
-public class JavaTheHuttApplication implements CommandLineRunner {
+public class JavaTheHuttApplication{
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(JavaTheHuttApplication.class, args);
 
-
-    }
-
-
-    @Override
-    public void run(String... args) {
 
     }
 }
