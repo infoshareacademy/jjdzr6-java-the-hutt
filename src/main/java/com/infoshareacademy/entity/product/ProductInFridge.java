35f54987dd23c2eee8906d6e258eabf4a2ff93bb
Przemyslaw Delimata <delimata.ps@gmail.com>
2022-09-26 20:15:44 +0200

    Create Enum ProductUnit. Add units to product classes. Add feature to choose unit in creating and editing recipes. Add feature to choose unit in adding prodcts to fridge.

--- src/main/java/com/infoshareacademy/entity/product/ProductInFridge.java
+++ src/main/java/com/infoshareacademy/entity/product/ProductInFridge.java
@@ -20,6 +20,10 @@ public class ProductInFridge extends Product {
     @Column(name = "amount")
     private Double amount;
 
+    @Column(name = "unit")
+    @Enumerated(EnumType.STRING)
+    private ProductUnit unit;
+
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "fridge_id")
     private Fridge fridge;
@@ -64,9 +68,20 @@ public class ProductInFridge extends Product {
         this.amount = quantity;
     }
 
+    public ProductUnit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(ProductUnit unit) {
+        this.unit = unit;
+    }
+
     @Override
     public String toString() {
-        return "nazwa składnika='" + productName + '\'' +
-                ", ilość=" + amount + "\'";
+        return "ProductInFridge{" +
+                "productName='" + productName + '\'' +
+                ", amount=" + amount +
+                ", unit=" + unit +
+                '}';
     }
 }
--- src/main/java/com/infoshareacademy/entity/product/ProductRecipe.java
+++ src/main/java/com/infoshareacademy/entity/product/ProductRecipe.java
@@ -18,6 +18,10 @@ public class ProductRecipe extends Product {
     @Column(name = "amount")
     private Double amount;
 
+    @Column(name = "unit")
+    @Enumerated(EnumType.STRING)
+    private ProductUnit unit;
+
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
     @JoinColumn(name = "recipe_id")
     private Recipe recipe;
@@ -62,9 +66,20 @@ public class ProductRecipe extends Product {
         this.amount = quantity;
     }
 
+    public ProductUnit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(ProductUnit unit) {
+        this.unit = unit;
+    }
+
     @Override
     public String toString() {
-        return "nazwa składnika='" + productName + '\'' +
-                ", ilość=" + amount + "\'";
+        return "ProductRecipe{" +
+                "productName='" + productName + '\'' +
+                ", amount=" + amount +
+                ", unit=" + unit +
+                '}';
     }
 }
