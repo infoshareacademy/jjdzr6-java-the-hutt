432f5709c98f55bd43889fdd3639b4b1fca93f43
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-10-06 19:46:39 +0200

    String value to meal in Recipe and FoodPreferences and unit in ProductUnit (enum)

--- src/main/java/com/infoshareacademy/entity/food_preferences/Meal.java
+++ src/main/java/com/infoshareacademy/entity/food_preferences/Meal.java
@@ -1,8 +1,18 @@
 package com.infoshareacademy.entity.food_preferences;
 
 public enum Meal {
-    BREAKFAST,
-    LUNCH,
-    DINNER,
-    SUPPER
+    BREAKFAST("śniadanie"),
+    LUNCH("lunch"),
+    DINNER("obiad"),
+    SUPPER("kolacja");
+
+    private final String value;
+
+    Meal(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
--- src/main/java/com/infoshareacademy/entity/product/ProductUnit.java
+++ src/main/java/com/infoshareacademy/entity/product/ProductUnit.java
@@ -1,11 +1,22 @@
 package com.infoshareacademy.entity.product;
 
 public enum ProductUnit {
-    MILIGRAM,
-    GRAM,
-    KILOGRAM,
-    MILILITR,
-    LITR,
-    PIECE,
-    PACK,
+    MILIGRAM("mg"),
+    GRAM("g"),
+    KILOGRAM("kg"),
+    MILILITR("ml"),
+    LITR("l"),
+    PIECE("szt."),
+    PACK("op."),
+    ;
+
+    private final String value;
+
+    ProductUnit(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
