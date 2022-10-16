83f9818babe3967427351d175e5b522117a9e863
Mikołaj Klimek <mikolaj.klimek.25@gmail.com>
2022-10-16 13:59:50 +0200

    Deleted CommandLineRunner from Main class

--- src/main/java/com/infoshareacademy/JavaTheHuttApplication.java
+++ src/main/java/com/infoshareacademy/JavaTheHuttApplication.java
@@ -1,30 +1,13 @@
 package com.infoshareacademy;
 
 
-import com.infoshareacademy.entity.product.ProductInFridge;
-import com.infoshareacademy.entity.recipe.Recipe;
-import com.infoshareacademy.repository.RecipeAllergensRepository;
-import com.infoshareacademy.repository.RecipeRepository;
-import com.infoshareacademy.service.FridgeService;
-import com.infoshareacademy.service.RecipeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
-public class JavaTheHuttApplication implements CommandLineRunner {
+public class JavaTheHuttApplication{
 
-    @Autowired
-    private RecipeService recipeService;
 
-    @Autowired
-    private RecipeAllergensRepository allergensRepository;
-
-    @Autowired
-    private FridgeService fridgeService;
 
     public static void main(String[] args) {
         SpringApplication.run(JavaTheHuttApplication.class, args);
@@ -32,15 +15,5 @@ public class JavaTheHuttApplication implements CommandLineRunner {
 
     }
 
-    @Override
-    public void run(String... args) throws Exception {
 
-//        List<Recipe> allRecipe = recipeService.getRecipeByProductsInFridge();
-//
-//        for(int i=0;i<allRecipe.size();i++){
-//            System.out.println(allRecipe.get(i).toString());
-//        }
-
-
-    }
 }
