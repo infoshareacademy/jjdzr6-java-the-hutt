a3cbdc77a05b09699535d57825fe7029336b159f
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-10-20 11:04:14 +0200

    Add deleteProductFromFridge() in FridgeServiceTest

--- src/test/java/com/infoshareacademy/service/FridgeServiceTest.java
+++ src/test/java/com/infoshareacademy/service/FridgeServiceTest.java
@@ -4,6 +4,7 @@ import com.infoshareacademy.entity.product.ProductInFridge;
 import com.infoshareacademy.entity.product.ProductUnit;
 import com.infoshareacademy.repository.FridgeRepository;
 import com.infoshareacademy.repository.ProductInFridgeRepository;
+import javassist.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -31,5 +32,13 @@ class FridgeServiceTest {
         assertNotNull(productAfterUpdate);
         assertEquals("Jabłka", productAfterUpdate.getProductName());
         assertEquals(ProductUnit.LITR, productAfterUpdate.getUnit());
+    }
+
+    @Test
+    void deleteProductFromFridge(){
+        //given
+        ProductInFridge product = new ProductInFridge("Jajka", 1.0, ProductUnit.GRAM, LocalDate.now());
+        //when and then
+        assertThrows(NotFoundException.class, () -> fridgeService.deleteProductFromFridge(product.getProductId()));
     }
 }
