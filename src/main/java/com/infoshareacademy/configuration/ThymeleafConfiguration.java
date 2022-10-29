c32f3cf6efc9a075bf6acc401cd9012d4ad0082f
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-10-29 11:06:48 +0200

    Move modelMapper() to ThymeleafConfiguratio and delete ModelMapperConfiguration.java

--- src/main/java/com/infoshareacademy/configuration/ThymeleafConfiguration.java
+++ src/main/java/com/infoshareacademy/configuration/ThymeleafConfiguration.java
@@ -1,5 +1,6 @@
 package com.infoshareacademy.configuration;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
@@ -10,5 +11,11 @@ public class ThymeleafConfiguration {
     @Bean
     public SpringDataDialect springDataDialect() {
         return new SpringDataDialect();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
     }
 }
